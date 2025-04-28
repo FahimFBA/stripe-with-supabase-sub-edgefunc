38edb194cd3b4067ac34531670652de1c6887a15
FahimFBA <fahimbinamin@gmail.com>
2025-04-28 17:06:16 +0600

    rm: all data

--- src/pages/cancel.tsx
+++
@@ -1,7 +0,0 @@
-export default function Cancel() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold text-red-600">Payment Cancelled ❌</h1>
-    </div>
-  );
-}
--- src/pages/success.tsx
+++
@@ -1,9 +0,0 @@
-export default function Success() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold text-green-600">
-        Payment Successful! 🎉
-      </h1>
-    </div>
-  );
-}
